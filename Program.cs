55d6bc32e245ecf27c4ccc669863fe6b7c38dd58
erikappelgren123@gmail.com <erikappelgren123@gmail.com>
2020-10-26 15:21:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examination_prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int personalNumber;
+            //ask user for personal number
+            Console.WriteLine("Skriv in ditt personnummer: ");
+            personalNumber = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            //12 numbers from 1753 - 2020
+            //month 1 - 12
+            //valid date to month 30 or 31
+            //last numbers nnn should be from 000-999
+            //control second to last number, even = male, uneven = female
+            //print correct personal number and if user is male or female
+        }
+    }
+}
